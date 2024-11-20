1283f1f5407de87c97ba128847787368c05c1027
Roy <qq3392313023@163.com>
2024-11-20 22:53:18 +0800

    course

--- main.go
+++ main.go
@@ -9,7 +9,7 @@ import (
 
 func main() {
 	utils.YatoriCoreInit()
-	cache := qitApi.UserCache{Account: "XXX", Password: "XXX"}
+	cache := qitApi.UserCache{Account: "学号", Password: "密码"}
 	loginAction, err := action.QitLoginAction(&cache)
 	if err != nil {
 		panic(err)
